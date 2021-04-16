043c56d385c6fbeedd2d204a68308940fbe10ab3
Soumeya <soumeyahassan47@gmail.com>
2021-04-16 15:28:06 +0100

    added results js

--- search-function/search.js
+++ search-function/search.js
@@ -42,13 +42,3 @@ const onSearch = () => {
 };
 
 $("#search-container").on("click", "a", onSearch);
-
-// const getUrlParams = () => {
-//   const urlParams = new URLSearchParams(window.location.search);
-//   const cityName = urlParams.get("cityName");
-//   const eventType = urlParams.get("eventType");
-
-//   console.log(cityName, eventType);
-// };
-
-// $(document).ready(getUrlParams);
